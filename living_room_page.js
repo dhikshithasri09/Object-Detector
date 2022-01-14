e5e5e3cb786f5ff8a6cb2b8a48cd92818afda481
dhikshithasri09 <79088313+dhikshithasri09@users.noreply.github.com>
2022-01-14 19:23:14 +0530

    Add files via upload

---
+++ living_room_page.js
@@ -0,0 +1,55 @@
+img = "";
+Status = "";
+objects = [];
+
+function preload(){
+    img = loadImage("Living_Room.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = "Detecting Objects";
+}
+
+function draw(){
+    image(img,0,0,640,420);
+
+    if(Status != ""){
+
+        objectDetector.detect(img,gotResult);
+
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML = "Objects Detected";
+            document.getElementById("status").innerHTML = "Number of Objects Detected are : " + objects.length;
+
+           fill("#000000");
+           percent = floor(objects[i].confidence*100);
+           text(objects[i].label + " " + percent + "%" + objects[i].x,objects[i].y);
+           noFill();
+           stroke("#000000");
+           rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("Model is Loaded !");
+    Status = true;
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+
+    console.log(results);
+    objects = results;
+}
+
+function back(){
+    window.location = "index.html";
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+function bedroom_picture()
+{
+    window.location = "bedroom_page.html";
+}
+
+function living_room_picture()
+{
+    window.location = "living_room_page.html";
+}
+
+function bottle_picture()
+{
+    window.location = "bottle_page.html";
+}
+
+function desk_picture()
+{
+    window.location = "desk_page.html";
+}
+
+function fruit_basket_picture()
+{
+    window.location = "fruit_basket_page.html";
+}
